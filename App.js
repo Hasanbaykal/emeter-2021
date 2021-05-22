f43bf08bf3898043596e40a5d02b260da2e017e3
HasanBaykal <baykalhh@gmail.com>
2021-05-22 20:40:25 +0200

    New Homepage / Loginpage
    - added secureTextEntry for hiding password with starts
    -

--- App.js
+++ App.js
@@ -1,22 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-
-import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
-
-export default class customfont extends Component {
-  render() {
+export default class App extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+  render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welkom, 
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{"\n"}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.logo}>HeyAPP</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+
+  
       </View>
     );
   }
@@ -25,23 +44,44 @@ export default class customfont extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  welcome: {
-    fontFamily: "KoHo-Bold",
-    fontSize: 30,
-    textAlign: "center",
-    margin: 10
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
   },
-  instructions: {
-    fontFamily: "KoHo-Light",
-    fontSize: 20,
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
   }
 });
-
-AppRegistry.registerComponent("customfont", () => customfont);
