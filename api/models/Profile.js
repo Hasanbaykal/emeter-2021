2ea4440c5689fe025350768f9c9de4a90aefd98d
twpjdb <44226640+twpjdb@users.noreply.github.com>
2021-06-08 12:20:59 +0200

    objectify profile schema and add required properties

--- api/models/Profile.js
+++ api/models/Profile.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    age: Number,
-    profile_picture: Image,
-    about_me: String
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    profile_picture: {
+        type: Image
+    },
+    about_me: {
+        type: String
+    }
 });
 
 module.exports = mongoose.model('Profile', ProfileSchema);
